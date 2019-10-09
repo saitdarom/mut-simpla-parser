1dd2ea609cf017e27ce3ab01c0eb492ef3b3fbe1
Alex <wolf-phantom@ya.ru>
2019-10-09 11:02:28 +0300

    правки к первой версии

--- src/Api/ServerApi.php
+++ src/Api/ServerApi.php
@@ -7,31 +7,37 @@ class ServerApi
 
     public function get_tree($category_id){
         $url=$this->domen.'/parsers/server/getTree.php?id='.$category_id;
-        return json_decode(file_get_contents($url));
+        $temp=json_decode(file_get_contents($url));
+        return $temp->return;
     }
 
     public function get_category($category_id){
         $url=$this->domen.'/parsers/server/getCategory.php?id='.$category_id;
-        return json_decode(file_get_contents($url));
+        $temp=json_decode(file_get_contents($url));
+        return $temp->return;
     }
 
     public function search_category($name){
         $url=$this->domen.'/parsers/server/getCategory.php?name='.$name;
-        return json_decode(file_get_contents($url));
+        $temp=json_decode(file_get_contents($url));
+        return $temp->return;
     }
 
     public function get_product($product_id){
         $url=$this->domen.'/parsers/server/getProduct.php?id='.$product_id;
-        return json_decode(file_get_contents($url));
+        $temp=json_decode(file_get_contents($url));
+        return $temp->return;
     }
     public function search_product($name){
         $url=$this->domen.'/parsers/server/getProduct.php?name='.$name;
-        return json_decode(file_get_contents($url));
+        $temp=json_decode(file_get_contents($url));
+        return $temp->return;
     }
 
     public function get_products($category_id,$page){
         $url=$this->domen.'/parsers/server/getProducts.php?id='.$category_id.'&page='.$page;
-        return json_decode(file_get_contents($url));
+        $temp=json_decode(file_get_contents($url));
+        return $temp->return;
     }
 
     public function get_seo_title($host,$productName,$server_id){
